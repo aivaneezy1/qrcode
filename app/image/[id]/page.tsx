00f15022d5aa3d4839c3925987210141f9cd8d84
aivaneezy1 <aivaneezydakii@gmail.com>
2025-11-22 00:41:33 +0100

    Fixed error

--- app/image/[id]/page.tsx
+++ app/image/[id]/page.tsx
@@ -5,7 +5,7 @@ interface Params {
 }
 
 export default async function Page({ params }: { params: Params }) {
-    const { id } = await params;
+    const { id } = params;
 
     const res = await fetch(
         `${process.env.NEXT_PUBLIC_BASE_URL}/api/image/${id}`,
